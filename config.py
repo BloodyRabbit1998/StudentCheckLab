4248bdecfe8ebd1a585a8f00d322c5cc60adff41
BloodyRabbit1998 <cool.goodboy1998@gmail.com>
2025-04-15 09:23:55 +0600

    fix .env

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
 from dotenv import dotenv_values
 
-env=dotenv_values(".env")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+env = dotenv_values(os.path.join(BASE_DIR, '.env'))
 
 URL_SQL=env["SQL_URL"]
 
